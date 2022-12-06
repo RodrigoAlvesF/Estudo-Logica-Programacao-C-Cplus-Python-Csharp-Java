efac17a2ecca66894b793105b19008e81f078624
RodrigoAlvesF <rodrigofidem@gmail.com>
2022-12-05 21:48:26 -0300

    5/12 - C# Finish

---
+++ Estudo C#/Exercicios/Matriz/Problema Diagonal Negativos/Problema Diagonal Negativos/Problema Diagonal Negativos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problema_Diagonal_Negativos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n, cont;
+
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+
+            Console.WriteLine("Diagonal principal: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+
+            cont = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    if (mat[i,j] < 0)
+                    {
+
+                        cont = cont + 1;
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Quantidade de negativos = " + cont);
+
+        }
+    }
+}
---
+++ Estudo C#/Exercicios/Vetor/Problema Soma Vetor/Problema Soma Vetor/Problema Soma Vetor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problema_Soma_Vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double soma, media;
+            int n;
+
+            Console.Write("Quantos números você vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double [n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("Digite um número: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+
+            }
+
+            Console.Write("Valores = ");
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write(vet[i].ToString("F1", CI) + " ");
+
+            }
+                      
+            soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                soma = soma + vet[i];
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Soma = " + soma.ToString("F2", CI));
+
+            media = soma / n;
+
+            Console.WriteLine();
+            Console.Write("Média = " + media.ToString("F2", CI));
+           
+
+
+        }
+    }
+}
