3585d14d7c5f8bf292ea3323a07b94ae61995e88
RodrigoAlvesF <rodrigofidem@gmail.com>
2022-12-06 22:28:52 -0300

    6/12 Start Python

---
+++ Estudo Python/Aula Variáveis/Programa.py
@@ -0,0 +1,13 @@
+idade = 20
+salario = 5800.5
+altura = 1.63
+genero = 'F'
+nome = "Maria Silva"
+
+print(f"idade = {idade}")
+print(f"Salario = {salario:.2f}")
+print(f"Altura = {altura:.2f}")
+print(f"Genero = {genero}")
+print(f"Nome = {nome}")
+
+
